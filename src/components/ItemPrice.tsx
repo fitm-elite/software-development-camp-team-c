246bfb2f03b2d057a61884ca737f24d89c67ec91
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-09-14 13:31:00 +0700

    feat(itemPrice): add interface

--- src/components/ItemPrice.tsx
+++ src/components/ItemPrice.tsx
@@ -1,12 +1,25 @@
-export default function ItemPrice() {
+interface ItemPriceProps {
+  price: number;
+  id: number;
+  removePriceList: (id: number) => void;
+}
+
+export default function ItemPrice({
+  price,
+  id,
+  removePriceList,
+}: ItemPriceProps) {
   return (
     <div className="m-2">
-      <div className="flex  justify-between  bg-gray-50 shadow-md px-3 py-4 rounded-md items-center">
+      <div className="flex  justify-between  bg-gray-50 shadow-md px-3 py-2 rounded-md items-center">
         <div>
-          <h5 className="font-mono text-xl font-bold ">200 ฿</h5>
+          <h5 className="font-mono text-2xl font-bold ">{price} ฿</h5>
         </div>
         <div>
-          <button className="text-white font-bold bg-red-500  rounded-md p-2">
+          <button
+            onClick={() => removePriceList(id)}
+            className="text-white font-bold bg-red-500  rounded-md px-4 py-3 hover:bg-red-600"
+          >
             X
           </button>
         </div>
