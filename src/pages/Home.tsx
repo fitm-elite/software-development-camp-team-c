7ca13d5a351d8bb3746af14e5c97feb579336746
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2024-09-14 11:03:32 +0700

    feat(non-functional): add PricingListItem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
-import Simple from "@/components/Simple";
+import PricingListItem from "@/components/PricingListItem";
 
 export default function Home() {
     return (
-        <div className="w-screen h-screen flex justify-center items-center bg-gray-100 space-x-4">
-            <div className="bg-white p-4 shadow-lg rounded-lg">
-                <h1 className="text-2xl font-bold text-center">Home</h1>
-                <p className="text-center">This is the home page.</p>
-            </div>
-
-            <Simple />
+        <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-100 gap-4 px-4">
+            { [40, 50, 10, 2].map((price) => <PricingListItem pricing={price} count={0} setCount={() => {}} />) }
         </div>
     )
 }
